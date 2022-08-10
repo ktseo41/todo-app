ea80c325715fa875ce99ef6a36a3fbe9ca45b5b9
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-08-10 21:02:49 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import InputBox from "input-box";
+import NoteList from "note-list";
+import { ref } from "vue";
+
+const text = ref("");
+const todos = ref([]);
+
+function addTodo() {
+  todos.value.push(text.value);
+  text.value = "";
+}
+</script>
+
+<template>
+  <main>
+    <InputBox v-model="text" @keydown.enter="addTodo"></InputBox>
+    <button @click="addTodo">Add</button>
+    <NoteList :todos="todos"></NoteList>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-bottom: 50vh;
+}
+</style>
